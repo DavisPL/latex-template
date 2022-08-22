e7225a19b3e5adf314b30f41d1ac0060f2fd0d6e
Caleb Stanford <caleb.pirsquared@gmail.com>
2022-08-22 19:01:02 -0400

    :sparkles: initialization script

---
+++ init.sh
@@ -0,0 +1,10 @@
+# Initialization script to use latex-tools with your repository.
+
+# TODO: check that repo is set up properly (has a src/ directory, no
+# data/ or build/ directories, etc.)
+
+echo "Setting up latex-tools..."
+
+cp latex-tools/Makefile .
+mkdir build
+mkdir data
